f5847c76dba302592b7aff78ba550d6fae4921d4
Adranuz <garciasaaib@gmail.com>
2024-06-20 14:58:25 -0700

    docs: :art: Adding default values in swagger web
    Adding default values for params in the endpoint of github commits

--- backend/src/github/github.controller.ts
+++ backend/src/github/github.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { GithubService } from './github.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('github')
 @Controller('github')
@@ -8,6 +8,26 @@ export class GithubController {
   constructor(private readonly githubService: GithubService) {}
 
   @ApiOperation({ summary: 'Get commits from a Github repository' })
+  @ApiParam({
+    name: 'user',
+    required: true,
+    description: 'The Github username',
+    example: 'garciasaaib',
+  })
+  @ApiParam({
+    name: 'repo',
+    required: true,
+    description: 'The Github repository name',
+    example: 'github-fullstack',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The list of commits has been successfully retrieved.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'The specified user or repository could not be found.',
+  })
   @Get(':user/:repo')
   findOne(@Param('user') user: string, @Param('repo') repo: string) {
     return this.githubService.findCommits(user, repo);
